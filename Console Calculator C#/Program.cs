eba28c04f2948442e31e0ea04587d47acdebb458
Sudharaka pathiraja <sudharakadulanjith200@gmail.com>
2024-11-08 01:09:01 +0530

    Add project files.

---
+++ Console Calculator C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console Calculater in C#");
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Type a number and then press enter:");
+            double no1 = Convert.ToDouble(Console.ReadLine()); //How to get users input
+
+            Console.WriteLine("Type another number and then press enter:");
+            double no2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose and option from the following list:");
+            Console.WriteLine("\t + ADD");
+            Console.WriteLine("\t - SUB");
+            Console.WriteLine("\t * MUL");
+            Console.WriteLine("\t / DIV");
+
+            Console.ReadKey();
+        }
+    }
+}
